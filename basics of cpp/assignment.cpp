2d80e3378c72b98581fbf419c8357b91309c2c87
my-user-name <me@gmail.com>
2025-03-18 22:23:47 +0530

    updated files

---
+++ basics of cpp/assignment.cpp
@@ -0,0 +1,118 @@
+//write a program that takes two integer as input and print their sum
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     cout<<"enter the first number"<<endl;
+//     cin>>a;
+//     cout<<"enter the second number"<<endl;
+//     cin>>b;
+//     cout<<"the um of the number is:"<<endl;
+//     c=a+b;
+//     cout<<c<<endl;
+    
+// }
+
+//take length and breadth as input(both integers)and calculate the area of rectangle
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int l,b,area;
+//     cout<<"enter the length :";
+//     cin>>l;
+//     cout<<"enter the breadth :";
+//     cin>>b;
+//     area=l*b;
+//     cout<<"area of rectangle is:"<<area;
+
+// }
+
+//take two numbers as input and make a calculator
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cin>>a;
+//     cin>>b;
+
+//     cout<<a+b<<endl;
+//     cout<<a-b<<endl;
+//     cout<<a*b<<endl;
+//     cout<<a/b<<endl;
+
+// }
+
+//take input of principle,time and rate and find simple interest
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float p,r,t;
+//     float si;
+//     cout<<"enter principle amount:";
+//     cin>>p;
+//     cout<<"enter rate:";
+//     cin>>r;
+//     cout<<"enter time:";
+//     cin>>t;
+//     cout<<"the simple interest is "<<endl;
+//     si=(p*r*t)/100;
+//     cout<<si;
+    
+// }
+
+//take a character as input and print its coreesponding ascii values
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch;
+//     cout<<"enter the character:";
+//     cin>>ch;
+
+//     cout<<(int)ch;
+// }
+
+//take a floating point number as input typecast it into an integer and print both values
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float a;
+//     cin>>a;
+//     cout<<a<<endl;
+//     cout<<int(a);
+
+// }
+
+//take an integer between 65 and 90 as input and print its corresponding uppercase letter
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter the number:";
+//     cin>>n;
+//     cout<<char(n);
+// }
+
+//predict the output
+#include<iostream>
+using namespace std;
+int main(){
+//     int a=4;
+//     int b=5;
+//     a++,b--;
+//     cout<<++a<<" "<<b--;
+
+//     return 0;
+
+// int x=5;
+// cout<<x++<<" "<<++x;
+
+// int y;
+// cin>>y;
+// cout<<y++;
+// cout<<++y;
+
+// int a=3,b=5,c;
+// c=a++ + ++b;
+// cout<<a<<" "<<b<<" "<<c;
+}
+
---
+++ basics of cpp/assignment1.cpp
@@ -0,0 +1,71 @@
+//write a program to find the euclidean distance between two coordinates.take both the coordinates from the user as input
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int main(){
+//     int x1,x2,y1,y2,dist;
+//     cin>>x1;
+//     cin>>x2;
+//     cin>>y1;
+//     cin>>y2;
+//     cout<<"the distance is:"<<sqrt(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+// }
+
+//write a program that will tell the number of dogs and chicken are there when the user will provide the value of total heads and legs
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int head,legs,c,d;
+//     cout<<"enter the number of head";
+//     cin>>head;
+//     cout<<"enter the number of legs";
+//     cin>>legs;
+//     c=(2*head-0.5*legs);
+//     d=head-c;
+//     cout<<c<<" "<<d;
+
+// }
+
+//write a program to find the sum of square of first n natural numbers where n will be provided by the user
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,sum;
+//     cout<<"enter the number";
+//     cin>>n;
+//     sum=(n*(n+1)*(2*n+1))/6;
+//     cout<<sum;
+    
+// }
+
+//given 2 fractions,find the sum of those 2 fractions.take the numerator and denominator values of the fractions from the user
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x1,x2,y1,y2,sum;
+//     cin>>x1;
+//     cin>>x2;
+//     cin>>y1;
+//     cin>>y2;
+//     sum=((x1*y2)+(x2*y1))/(x2*y2);
+//     cout<<sum;
+// }
+
+//predict the output
+#include<iostream>
+using namespace std;
+int main(){
+//     int a=20,b=6,c=4;
+//     cout<<a%b+c%a;
+//     return 0;
+
+// int a=10;
+// int b=5;
+// int c=a +=b -=2;
+// cout<<a<<" "<<b<<" "<<c;
+
+
+
+}
+
